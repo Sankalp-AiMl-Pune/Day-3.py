0f52a0d90e7a40a2578c3de0b354484c754dad02
Sankalp-AiMl-Pune <sankalp.lokhande321@gmail.com>
2026-08-29 14:45:27 +0530

    Create Day-4.py

---
+++ Day-4.py
@@ -0,0 +1,8 @@
+# Day 4 - Simple Calculator
+num1 = int(input("Pehla number dal: "))
+num2 = int(input("Dusra number dal: "))
+
+print(f"Jod = {num1 + num2}")
+print(f"Ghatav = {num1 - num2}")
+print(f"Guna = {num1 * num2}")
+print(f"Bhaag = {num1 / num2}")
