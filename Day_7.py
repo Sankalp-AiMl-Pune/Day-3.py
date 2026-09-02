bec14db79c8c0d27daf40ecf19d61c46a25c77c9
Sankalp-AiMl-Pune <sankalp.lokhande321@gmail.com>
2026-09-02 11:47:08 +0530

    Create Day_7.py

---
+++ Day_7.py
@@ -0,0 +1,10 @@
+# Day 7 - Loop + Progress Tracker
+# Author: Sankalp
+
+days_completed = 7
+print(f"Day {days_completed} Completed! 🔥")
+
+for i in range(1, 8):
+    print(f"Day {i} - Done ✅")
+
+print("1 week streak! No stopping now.")
